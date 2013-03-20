22744b2dc7efb4e87f4517213ac57f7f2662f36f
manuel <manueltorovelez@gmail.com>
2013-03-20 16:05:40 -0500

    cambio nombre index por send

---
+++ send.php
@@ -0,0 +1,15 @@
+<?php
+include 'CommentCheckClass.php';
+/**
+ * Created by JetBrains PhpStorm.
+ * User: User
+ * Date: 3/17/13
+ * Time: 9:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$code = "blahblahbla";
+$codec = new CommentCheck($code);
+$codec->verify();
+print_r($codec->getErrors());
+
