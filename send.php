ad905a19807dff95b8b07bf600d8938aa7ed4595
manuel <manueltorovelez@gmail.com>
2013-03-20 21:26:37 -0500

    arreglo send

--- send.php
+++ send.php
@@ -7,15 +7,15 @@ include 'CommentCheckClass.php';
  * Time: 9:36 PM
  * To change this template use File | Settings | File Templates.
  */
- if (isset ( $_POST ['submit'] )) {
-	    if (isset ( $_FILES ['archivo'] ) && is_uploaded_file ( $_FILES ['archivo'] ['tmp_name'] )) {
-			chmod($_FILES['archivo']['tmp_name'], 0444);
-			$code=file_get_contents($_FILES['archivo']['tmp_name']);
+		if (isset ( $_POST ['submit'] )) {
+			 if (isset ( $_FILES ['file'] ) && is_uploaded_file ( $_FILES ['file'] ['tmp_name'] )) {
+			chmod($_FILES['file']['tmp_name'], 0444);
+			$code=file_get_contents($_FILES['file']['tmp_name']);
 			$codec = new CommentCheck($code);
 			$codec->verify();
 			print_r($codec->getErrors());
-		}else{
-			echo 'ese hp no subio';
+			}
 		}
-
-	}
+		
+		$bio = $_POST['code'];
+		echo $bio;
