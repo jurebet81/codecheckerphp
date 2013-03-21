329fdba0ee9adb47f926f64e327b2b8b8d62a241
manuel <manueltorovelez@gmail.com>
2013-03-21 11:53:37 -0500

    organizo ambos archivos

--- send.php
+++ send.php
@@ -7,15 +7,19 @@ include 'CommentCheckClass.php';
  * Time: 9:36 PM
  * To change this template use File | Settings | File Templates.
  */
-		if (isset ( $_POST ['submit'] )) {
+		if (isset ( $_POST ['submitfile'] )) {
 			 if (isset ( $_FILES ['file'] ) && is_uploaded_file ( $_FILES ['file'] ['tmp_name'] )) {
 			chmod($_FILES['file']['tmp_name'], 0444);
 			$code=file_get_contents($_FILES['file']['tmp_name']);
+			}
+		}elseif ($_POST['submitcode'] ) {
+		
+			$code = $_POST['code'];
+			echo $code;
+		}
+		
 			$codec = new CommentCheck($code);
 			$codec->verify();
 			print_r($codec->getErrors());
-			}
-		}
-		
-		$bio = $_POST['code'];
-		echo $bio;
+	
+ 
