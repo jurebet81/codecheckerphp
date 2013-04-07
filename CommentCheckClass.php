9141096b2453f91d5067151055610890671ae3c8
julian <julianrpo2711@hotmail.com>
2013-04-06 19:32:33 -0500

    se corrige metodos que no funcionaron

--- CommentCheckClass.php
+++ CommentCheckClass.php
@@ -9,7 +9,7 @@
  */
 
 /**
- * CommentChek
+ * CommentCheck
  *
  * Review code errors and return a vector with them
  * 
@@ -26,12 +26,17 @@ Class CommentCheck {
     private $tags;
     private $bottom;
 
+    private $spaces;
+    private $lineArray;
+    private $numCharactersLine;
+    private $ascii;
+
     /**
-     * Constructor
+     * constructor
      *
      * Gets the code as a string and calls the method to put it into an array
      * Calls the method to find how many classes and methods there are in the php file
-     *
+    *
      * @param (string)($code)
      */
     public function __construct($code, $tags) {
@@ -50,63 +55,82 @@ Class CommentCheck {
      */
     public function verify() {
         for ($i = 0; $i < $this->getNumTimes(); $i++) {
+            $this->setSpaces(0);
             $this->setPosition(($this->positions[$i]));
-           // $this->align();
-            //$this->setLastLine(false);
-           
-           
-            
+            $this->setSpaces($this->codeArray[$this->positions[$i]]);
 			$this->setPosition(($this->positions[$i]) - 1);
-				 $this->setBottom(($this->getPosition())+1);
-		
+		    $this->setBottom(($this->getPosition())+1);
             $this->checkBottom();
-//                $this->checkBlankLine();
+        }
+    }
+
+/**
+    private function hasComment(){
+        $n = $this->getPosition();
+        if ($n < 2){
+            $this->setErrors('The Class: ' . $this->codeArray[$this->getPosition()+1] . ', does not have its comment');
+        }else {
+            for ($i=$n;$i>1;$i--){
+                if ($this->codeArray[$i] ){
+
+                }
+            }
+
         }
     }
 
     /**
-      checkBottom
+     * checkBottom
      *
      * Checks the bottom of each comment
      *
      */
+
     private function checkBottom() {
         $this->setFlag(0);
-        if (preg_match('/^([\s]*)(\*)(\/)([\s]*)$/', $this->codeArray[($this->getPosition())])) {
-//                 $this->align();
+        if ($this->getPosition() > 2){
+            while ((empty($this->codeArray[($this->getPosition())])) ||
+            (preg_match('/^([\s]+)$/', $this->codeArray[($this->getPosition())])) ){
             $this->setPosition($this->getPosition() - 1);
-        $this->checkMiddle();
-        } else {
-            $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not commented properly => '
-                    . $this->codeArray[$this->getPosition()]);
+            $this->setErrors('Line: ' . ($this->getPosition() + 2 )  . ', can not be empty');
+            }
+
+            if ( preg_match('/(\s}|[}])+/', $this->codeArray[($this->getPosition())])==false ){
+                if ( preg_match('/^([\s]*)(\*)(\/)([\s]*)$/', $this->codeArray[($this->getPosition())])) {
+                    $this->checkAlign($this->codeArray[($this->getPosition())], $this->getSpaces());
                     $this->setPosition($this->getPosition() - 1);
-        $this->checkMiddle();
+                    $this->checkMiddle();
+                } else {
+                    $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not commented properly => '
+                            . $this->codeArray[$this->getPosition()]);
+                    $this->setPosition($this->getPosition() - 1);
+                    //$this->checkMiddle();
+                }
+            }else{
+                $this->setErrors('The method: ' . $this->codeArray[$this->getPosition()+2] . ', does not have its comment');
+            }
+        }else{
+            $this->setErrors('The Class: ' . $this->codeArray[$this->getPosition()+1] . ', does not have its comment');
         }
-        
     }
 
     /**
      * checkMiddle
      *
-     * Checks the middle of the each comment and its beginning
+     * Checks the middle of the each comment
      *
      */
     private function checkMiddle() {
         if (( preg_match('/^([\s]*)(\*)+(.)*/', $this->codeArray[$this->getPosition()]))
                 && ( strpos($this->codeArray[$this->getPosition()], "*/") === false)) {
-           
             $this->setFlag($this->getFlag() + 1);
+            $this->checkAlign($this->codeArray[($this->getPosition())], $this->getSpaces());
+
             $this->setPosition($this->getPosition() - 1);
-           // $this->align();
             $this->checkMiddle();
         } else if (preg_match('/^([\s]*)\/(\*)+$/', $this->codeArray[$this->getPosition()]) && ( $this->getFlag() > 0)) {
-            // } else if (preg_match('/^([\s]*)\/([\*]{2})([\s]*)$/', $this->codeArray[$this->getPosition()]) && ( $this->getFlag() > 0)) {
-            //       $this->setLastLine(true);
-            //      $this->align();
-            // unset($this->alignArray); 
             $this->checkTop();
         } else {
-
             $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not commented properly => '
                     . $this->codeArray[$this->getPosition()]);
             $this->setPosition($this->getPosition() - 1);
@@ -114,14 +138,20 @@ Class CommentCheck {
         }
     }
 
+    /**
+     * checkTop
+     *
+     * Checks the top of the each comment
+     *
+     */
     private function checkTop() {
         if (!(preg_match('/^([\s]*)\/([\*]{2})([\s]*)$/', $this->codeArray[$this->getPosition()]))) {
             $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not commented properly => '
                     . $this->codeArray[$this->getPosition()]);
         }
-//              $this->setLastLine(true);
-//                $this->align();
-//         unset($this->alignArray); 
+        $this->checkAlign($this->codeArray[($this->getPosition())], $this->getSpaces());
+        $this->setPosition($this->getPosition() - 1);
+        $this->checkBlankLine();
         $this->checkTags();
     }
 
@@ -131,7 +161,6 @@ Class CommentCheck {
      * Sets positions into an array where the methods and classes were found
      *
      */
-
     private function setPositions() {
         for ($i = 0; $i < $this->getNumCodeLines(); $i++) {
             if (strpos($this->codeArray[$i], 'Class') !== false || strpos($this->codeArray[$i], 'public') !== false
@@ -148,12 +177,9 @@ Class CommentCheck {
      * commented on the comment.
      */
     private function checkTags() {
-
         $contAuthor = 0;
         $contReturn = 0;
         $contParam = 0;
-
-
         $tagArray = $this->tags;
         $position = $this->getPosition();
         $length = sizeof($this->getTags());
@@ -171,9 +197,6 @@ Class CommentCheck {
             }
 
             if ($tagArray[$j] == 'author') {
-                // echo '<p>Position: '.($this->getPosition()+1).'</p>';
-                //  echo '<p>Bottom: '.($this->getBottom()+1).'</p>';
-                //  echo '<p> array: '.$this->codeArray[($this->bottom) ].'</p>';
 
                 for ($m = $this->getPosition(); $m < $this->bottom; $m++) {
                     if ((preg_match('/([\s]*)[\s]((author)|(Author)|(@author))(:)?.+/', $this->codeArray[$m + 1]))) {
@@ -212,68 +235,48 @@ Class CommentCheck {
     /**
      * checkBlankLine
      *
-     * Verify each class and funtion in the code has a blank line before 
+     * Verify each comment in the code has a blank line before
      *
      */
     private function checkBlankLine() {
-        if (empty($this->codeArray[$this->getPosition()])) {
+        if ($this->getPosition()>1){
             $x = $this->getPosition() + 1;
-            $this->setErrors('Line: ' . $x . ', there is not a black line between the comment and the class');
-            //   $this->checkBottom();
-             $this->setPosition($this->getPosition() - 1);
-             $this->checkBottom();
-        } else {          
-            $this->checkBottom();
+            if (!empty($this->codeArray[$this->getPosition()])) {
+                $this->setErrors('Line: ' . $x . ', there is not a black line above the comment');
+            }
         }
     }
 
-    /**
-     * align
-     *
-     * Calculate spaces and tabs at the beginning of each class,funtion or comment line 
-     *
-     */
-    private function align() {
-        $this->setLineArray($this->codeArray[$this->getPosition()]);
-        $this->setNumCharactersLine(sizeof($this->getLineArray()));
-        for ($x = 0; $x < $this->getNumCharactersLine(); $x++) {
-            $this->setAscii(ord($this->lineArray[$x]));
-            if ($this->getAscii() == 32) {
-                $this->setCountSpaces($this->getCountSpaces() + 1);
-            } elseif ($this->getAscii() == 9) {
-                $this->setCountSpaces($this->getCountSpaces() + 4);
-            } else {
-                $x = $this->getNumCharactersLine();
-            }
-            $this->setAscii(0);
-        }
-        $this->setAlignArray($this->getCountSpaces());
-        $this->setCountSpaces(0);
-        $this->checkAlign();
+    private function checkProportion(){
+
     }
 
     /**
      * checkAlign
      *
-     * Compare spaces of the class or funtion with each line of the comment
+     * Compare spaces of the class or function with each line of the comment
      * The beginning of the comment must be the same position as the class or funtion
      *
-     */
-    private function checkAlign() {
-        //echo $lastLine;
-        $x = 0;
-        $x = sizeof($this->getAlignArray());
-        if ($x == 1) {
-            $this->setAligner($this->alignArray[$x - 1] + 1);
-        } elseif ($this->alignArray[$x - 1] != $this->getAligner()) {
-            if (!(($this->getLastLine() == true) && (($this->alignArray[$x - 1] + 1) == $this->getAligner() ))) {
-                $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not aligned with the class');
+    */
+    private function checkAlign($codeLine,$spaces) {
+        $spacesComment = 0;
+        $this->setLineArray($codeLine);
+        $this->setNumCharactersLine(sizeof($this->getLineArray()));
+        for ($x = 0; $x < $this->getNumCharactersLine(); $x++) {
+            $this->setAscii(ord($this->lineArray[$x]));
+            if ($this->getAscii() == 32) {
+                $spacesComment = $spacesComment + 1;
+            } elseif ($this->getAscii() == 9) {
+                $spacesComment = $spacesComment + 4;
+            } else {
+                $x = $this->getNumCharactersLine();
             }
         }
-    }
 
-    private function checkProportion() {
-        
+        if ($spacesComment <> $spaces){
+            $this->setErrors('Line: ' . ($this->getPosition() + 1) . ', is not aligned with the class');
+        }
+
     }
 
     public function setPosition($position) {
@@ -360,14 +363,6 @@ Class CommentCheck {
         return $this->lineArray;
     }
 
-    public function setAlignArray($alignArray) {
-        $this->alignArray[] = $alignArray;
-    }
-
-    public function getAlignArray() {
-        return $this->alignArray;
-    }
-
     public function setNumCharactersLine($numCharactersLine) {
         $this->numCharactersLine = $numCharactersLine;
     }
@@ -384,29 +379,35 @@ Class CommentCheck {
         return $this->ascii;
     }
 
-    public function setCountSpaces($countSpaces) {
-        $this->countSpaces = $countSpaces;
+    /**
+     * setSpaces
+     *
+     * Calculate spaces and tabs at the beginning of each class,function or comment line
+     *
+     *
+     */
+    public function setSpaces($codeLine)
+    {
+        $this->setLineArray($codeLine);
+        $this->setNumCharactersLine(sizeof($this->getLineArray()));
+        for ($x = 0; $x < $this->getNumCharactersLine(); $x++) {
+            $this->setAscii(ord($this->lineArray[$x]));
+            if ($this->getAscii() == 32) {
+                $this->spaces = $this->getSpaces() + 1;
+            } elseif ($this->getAscii() == 9) {
+                $this->spaces = $this->getSpaces() + 4;
+            } else {
+                $x = $this->getNumCharactersLine();
+            }
+        }
+
     }
 
-    public function getCountSpaces() {
-        return $this->countSpaces;
+    public function getSpaces()
+    {
+        return $this->spaces;
     }
 
-    public function setAligner($aligner) {
-        $this->aligner = $aligner;
-    }
-
-    public function getAligner() {
-        return $this->aligner;
-    }
-
-    public function setLastLine($lastLine) {
-        $this->lastLine = $lastLine;
-    }
-
-    public function getLastLine() {
-        return $this->lastLine;
-    }
 
 }
 
